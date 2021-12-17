c4dc93ad70d0660f6826e392b2af188253e3ad25
YakmpovDmitry <dmitrijakimov569@gmail.com>
2021-12-17 16:34:16 +0400

    2

--- app/src/main/java/com/example/spacegame/MainActivity.java
+++ app/src/main/java/com/example/spacegame/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.spacegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new MyDraw(this));
+    }
+}
+
+class MyDraw extends View {
+    public MyDraw (Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int blue = Color.BLUE;
+        Paint paint = new Paint();
+        paint.setColor(blue);
     }
 }
