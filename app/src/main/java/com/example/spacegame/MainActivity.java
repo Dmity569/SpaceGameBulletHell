b4e73d278500057341539001949f2a21e20db0ca
YakmpovDmitry <dmitrijakimov569@gmail.com>
2021-12-17 17:36:15 +0400

    PLAY

--- app/src/main/java/com/example/spacegame/MainActivity.java
+++ app/src/main/java/com/example/spacegame/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.spacegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -16,20 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(new MyDraw(this));
+        setContentView(R.layout.activity_main);
+//        setContentView(new MyDraw(this));
     }
 }
 
-class MyDraw extends View {
-    public MyDraw (Context context) {
-        super(context);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        int blue = Color.BLUE;
-        Paint paint = new Paint();
-        paint.setColor(blue);
-    }
-}
+//class MyDraw extends View {
+//    public MyDraw (Context context) {
+//        super(context);
+//    }
+//
+//    @Override
+//    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+//        int blue = Color.BLUE;
+//        Paint paint = new Paint();
+//        // Выводим изображение
+//        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.spase2);
+//        int xx = canvas.getWidth(), yy = canvas.getHeight();
+//        canvas.drawBitmap(image, xx - image.getWidth(), yy - image.getHeight(), paint);
+//    }
+//}
