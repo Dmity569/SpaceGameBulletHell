135c400a23673a9a9de69ac0e8b04340794789e7
Dmitry <dmitrijakimov569@gmail.com>
2022-04-09 17:23:43 +0400

    reators activity volume merged with settings

--- app/src/main/java/com/example/spacegame/CreatorsActivity.java
+++ app/src/main/java/com/example/spacegame/CreatorsActivity.java
@@ -1,5 +1,7 @@
 package com.example.spacegame;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Window;
@@ -8,18 +10,27 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreatorsActivity extends AppCompatActivity {
-
+    SharedPreferences mSettings;
     MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mSettings = getSharedPreferences("mysettings", Context.MODE_PRIVATE);
+        float volume = (float) mSettings.getInt("music", 50);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_creators);
         mp = MediaPlayer.create(this, R.raw.creators_main_frame);
         mp.setLooping(true);
         mp.seekTo(0);
-        mp.setVolume(1.0f, 1.0f);
+
+        int MAX_VOLUME = 100;
+
+        final float volume_x = (float) (1 - (Math.log(MAX_VOLUME - volume) / Math.log(MAX_VOLUME)));
+        mp.setVolume(volume_x, volume_x);
+
         mp.start();
     }
 
