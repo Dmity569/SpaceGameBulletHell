616f39c939ed08d7cee8e28e408018fa6dc3df33
Lockrime-ch <artem163ch@gmail.com>
2022-04-27 19:22:20 +0400

    (almost) working ship shop

--- app/src/main/java/com/example/spacegame/EditorInfo.java
+++ app/src/main/java/com/example/spacegame/EditorInfo.java
@@ -1,15 +1,26 @@
 package com.example.spacegame;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+
 public class EditorInfo {
-    static int ship_amount = 1;
+    static int ship_amount = 2;
     public static class ShipType {
-        float max_health;
-        float def;
-        float e_def;
+        static float max_health;
+        static float def;
+        static float e_def;
+        static int sprite;
     }
     public static class Ship_Arrow extends ShipType{
-        float max_health = 100;
-        float def = 10;
-        float e_def = 10;
+        static float max_health = 100;
+        static float def = 10;
+        static float e_def = 10;
+        static int sprite = R.drawable.ship_arrow;
+    }
+    public static class Ship_Pure_Trident extends ShipType{
+        static float max_health = 80;
+        static float def = 20;
+        static float e_def = 20;
+        static int sprite = R.drawable.ship_pure_trident;
     }
 }
--- app/src/main/java/com/example/spacegame/ShipsActivity.java
+++ app/src/main/java/com/example/spacegame/ShipsActivity.java
@@ -4,20 +4,41 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import com.example.spacegame.EditorInfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShipsActivity extends AppCompatActivity {
     SharedPreferences mSettings;
     TextView moneyShips;
+    TextView shipInfo;
+    ImageView shipImage;
     LinearLayout shipButtons;
-    EditorInfo.ShipType shipType;
+    // EditorInfo.ShipType shipType;
+    String infoText = "ShipInfo";
+    //Ship Stat Bloc
+    float max_health;
+    float def;
+    float e_def;
+    int sprite;
+
+
+    private void changeShips() {
+        infoText = "Ship Info";
+        infoText += "\nHull (Health): " + Float.toString(max_health);
+        infoText += "\nArmour (Defence): " + Float.toString(def);
+        infoText += "\nShields (Energy Defence): " + Float.toString(e_def);
+        shipInfo.setText(infoText);
+        shipImage.setImageResource(sprite);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +47,9 @@ public class ShipsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ships);
         mSettings = getSharedPreferences("mysettings", Context.MODE_PRIVATE);
         moneyShips = (TextView) findViewById(R.id.moneyShips);
-        moneyShips.setText(Integer.toString(mSettings.getInt("money", 0)) + " G");
+        moneyShips.setText(Integer.toString(mSettings.getInt("money", 0)) + "   G");
+        shipInfo = (TextView) findViewById(R.id.shipInfo);
+        shipImage = (ImageView) findViewById(R.id.shipImage);
         shipButtons = (LinearLayout) findViewById(R.id.shipButtons);
         for (int i = 0; i < EditorInfo.ship_amount; i++){
             Button ship_button = (Button) shipButtons.getChildAt(i);
@@ -35,9 +58,19 @@ public class ShipsActivity extends AppCompatActivity {
                 public void onClick(View view) {
                     switch ((String) ship_button.getText()){
                         case "Arrow":
-                            shipType = new EditorInfo.Ship_Arrow();
+                            max_health = EditorInfo.Ship_Arrow.max_health;
+                            def = EditorInfo.Ship_Arrow.def;
+                            e_def = EditorInfo.Ship_Arrow.e_def;
+                            sprite = EditorInfo.Ship_Arrow.sprite;
+                            break;
+                        case "Pure\nTrident":
+                            max_health = EditorInfo.Ship_Pure_Trident.max_health;
+                            def = EditorInfo.Ship_Pure_Trident.def;
+                            e_def = EditorInfo.Ship_Pure_Trident.e_def;
+                            sprite = EditorInfo.Ship_Pure_Trident.sprite;
                             break;
                     }
+                    changeShips();
                 }
             }
                 );
